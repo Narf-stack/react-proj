9fa1ffa15351e9697789d5d14a13aa38d226dad5
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-03-16 20:59:02 +0100

    Delete tours + Refresh

--- tour-operator/src/App.js
+++ tour-operator/src/App.js
@@ -7,6 +7,10 @@ function App() {
   const [loading,setLoading] = useState(true)
   const [tours,setTours] = useState([])
 
+  const removeTour=(id)=>{
+    const newTours = tours.filter((tour)=> tour.id !== id)
+    setTours(newTours)
+  }
   const fetchTours = async() =>{
     setLoading(true)
     try {
@@ -30,8 +34,17 @@ function App() {
     </main>
   }
 
+  if(tours.length === 0){
+    return ( <main>
+      <div className='title'>
+        <h2>no tours left </h2>
+        <button className='btn' onClick={()=> fetchTours()}> Refresh</button>
+      </div>
+      </main>
+    )
+  }
   return <main>
-      <Tours tours={tours}/>
+      <Tours tours={tours} removeTour={removeTour}/>
     </main>
 }
 
