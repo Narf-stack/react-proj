70d9c15001b43cb1638a40fe13a85a7512962761
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-03-16 18:34:48 +0100

    Add Bootstrap + fetch/clear logic

--- bday-reminder/src/App.js
+++ bday-reminder/src/App.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
   const [people, setPeople] = useState(data)
   return <main>
     <section className='container'>
       <h3> {people.length} birthdays today</h3>
       <List people={people}/>
-      <button onClick={()=>console.log('clicked')}>
-        Clear all
-      </button>
+      <div className="d-flex">
+        <button className='btn-width' onClick={()=>setPeople([])}>
+          Clear all
+        </button>
+        <button className='btn-width' onClick={()=>setPeople(data)}>
+          Fetch all
+        </button>
+      </div>
     </section>
   </main>
 }
