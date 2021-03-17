ba72822b292853fc72fddd3e48e75aa3851056b0
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-03-17 20:54:29 +0100

    Tabs filtering

--- tabs/src/App.js
+++ tabs/src/App.js
@@ -26,8 +26,44 @@ function App() {
     </section>
     )
   }
-
-  return <h2>jobs</h2>
+  const { company, dates, duties,title } = jobs[value]
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>expe</h2>
+        <div className='under-line'></div>
+      </div>
+      <div className='jobs-center'>
+        <div className='btn-container'>
+          {
+            jobs.map((item,index)=>{
+              return(
+                <button key={item.id}
+                  onClick={()=> setValue(index)}
+                  className={`job-btn ${index === value && 'active-btn'}`}
+                >
+                  {item.company}
+                </button>
+              )
+            })
+          }
+        </div>
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty,index)=>{
+            return(
+              <div key={index} className='job-desc'>
+                <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default App
