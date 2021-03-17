ee9f3a96b18fa22b8147e53f84791140dae3a9aa
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-03-17 16:02:58 +0100

    Random Pers + Update test on new Index

--- reviews/src/Review.js
+++ reviews/src/Review.js
@@ -5,20 +5,39 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const Review = () => {
   const [index,setIndex]=useState(2)
   const {name,job,image,text}=people[index]
+
+  const checkNumber = (num) => {
+    if(num > people.length - 1){
+      return 0;
+    }
+    if(num < 0){
+      return people.length -1;
+    }
+    return num
+  }
+
   const nextPers =(()=>{
     setIndex((index)=>{
-      let newIndex = index >= people.length - 1 ? 0 : index + 1
-      return newIndex
+      let newIndex = index + 1
+      return checkNumber(newIndex)
     })
   })
 
   const prevPers =(()=>{
     setIndex((index)=>{
-      let newIndex = index === 0 ? people.length - 1 : index - 1
-      return newIndex
+      let newIndex = index - 1
+      return checkNumber(newIndex)
+
     })
   })
 
+  const randPers=()=>{
+    let randomIndex = Math.floor(Math.random()*people.length)
+    if(randomIndex === index ){
+      randomIndex = index + 1
+    }
+    setIndex(checkNumber(randomIndex))
+  }
   return <article className='review'>
     <div className='img-container'>
       <img src={image} alt={name} className='person-img' />
@@ -37,7 +56,7 @@ const Review = () => {
         <FaChevronRight />
       </button>
     </div>
-    <button className='random-btn'>
+    <button className='random-btn'onClick={randPers} >
       Serendipity
     </button>
   </article>;
