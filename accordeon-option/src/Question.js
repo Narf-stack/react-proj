0e60dc6ed543c0e34bf308f893163c4f4ea4f696
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-03-17 16:47:31 +0100

    Display single question

--- accordeon-option/src/Question.js
+++ accordeon-option/src/Question.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({title,info}) => {
+
+  const [readMore, setReadMore] = useState(false)
+  const readHide=()=>{
+    setReadMore(!readMore)
+  }
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={readHide}> btn</button>
+      </header>
+      <p>{readMore? info: `${info.substring(0,0)}`}</p>
+    </article>
+  )
 };
 
 export default Question;
