ef956f0dba029e99dd7e655441c10020eb980e51
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-03-18 19:26:41 +0100

    Structure

--- llorem-generator/src/App.js
+++ llorem-generator/src/App.js
@@ -1,10 +1,33 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
+  const [count, setCount]= useState(0)
+  const [text,setText]= useState([])
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log('hello')
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
+  <section className='section-center'>
+    <h3>tired of boring lorem ?</h3>
+    <form className='lorem-form' onSubmit={handleSubmit}>
+      <label htmlFor="amount">
+        how many paragraphs:
+      </label>
+      <input type="number" name="amount"
+        id="amount"
+        value={count}
+        onChange={(e)=>setCount(e.target.value)}
+      />
+      <button type='submit' className='btn'>
+        generate
+      </button>
+    </form>
+    <article className='lorem-text'>
+    </article>
+  </section>
     )
 }
 
