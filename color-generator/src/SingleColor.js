af6d83611694f241e04a0faca1f4c7bed2197db8
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-03-18 21:17:46 +0100

    Copy #hex on click

--- color-generator/src/SingleColor.js
+++ color-generator/src/SingleColor.js
@@ -6,9 +6,17 @@ const SingleColor = ({index,rgb,weight}) => {
   const bcg = rgb.join(',')
   const hex = rgbToHex(...rgb)
   return (
-    <article className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor:`rgb(${bcg})`}}>
+    <article
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{backgroundColor:`rgb(${bcg})`}}
+      onClick={()=>{
+        setAlert(true)
+        navigator.clipboard.writeText(hex)
+      }}
+      >
       <p className='percent-value'>{weight}%</p>
       <p className='color-value'>{hex}</p>
+      {alert && <p className='arlert'>Copied to clipboard</p>}
     </article>
   )
 
