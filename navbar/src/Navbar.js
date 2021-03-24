07674d87803d0da79556b866836f35abc48fd8ac
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-03-24 21:35:20 +0100

    Optimize nav height on toggle

--- navbar/src/Navbar.js
+++ navbar/src/Navbar.js
@@ -4,16 +4,31 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <nav>
+  const [showLinks, setShowLinks] = useState(false)
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
+
+  useEffect(()=>{
+    const linksHeight = linksRef.current.getBoundingClientRect().height
+    if(showLinks){
+      linksContainerRef.current.style.height = `${linksHeight}px`
+    } else{
+      linksContainerRef.current.style.height = '0px'
+    }
+  },[showLinks])
+  // <div className={`${showLinks?'show-container links-container':
+  // 'links-container'}`}>
+  return (
+    <nav>
       <div className='nav-center'>
         <div className='nav-header'>
           <img src={logo} alt="logo navbar" />
-          <button className='nav-toggle'>
+          <button className='nav-toggle' onClick={()=>setShowLinks(!showLinks)}>
             <FaBars />
           </button>
         </div>
-        <div className='links-container show-container'>
-          <ul className='links'>
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
             {
               links.map((link)=>{
                 const {id,url,text} = link
@@ -41,6 +56,7 @@ const Navbar = () => {
         </ul>
       </div>
     </nav>
+    )
 }
 
 export default Navbar
