b5aea919cc5194e51b4efea44aae18ccd3b18cc2
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-03-25 17:35:32 +0100

    Sidebar,modal open/close

--- sidebar-modal/src/Modal.js
+++ sidebar-modal/src/Modal.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
+import {useGlobalContext} from './context'
+
 const Modal = () => {
-  return <div className={`modal-overlay `}>
+  const {isModalOpen,closeModal} = useGlobalContext()
+
+  return <div className={`${isModalOpen?
+    'show-modal modal-overlay':'modal-overlay'} `}>
       <div className='modal-container'>
         <h3>modal content</h3>
-        <button className='clo-modal-btn'>
+        <button className='clo-modal-btn' onClick={closeModal}>
           <FaTimes />
         </button>
       </div>
