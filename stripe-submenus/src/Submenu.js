1b918179c249ac9f5fab5f21606b677a7dd0cf21
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-03-25 21:39:00 +0100

    Auto sizing submenu length depending of links

--- stripe-submenus/src/Submenu.js
+++ stripe-submenus/src/Submenu.js
@@ -4,18 +4,27 @@ import {useGlobalContext} from './context'
 const Submenu = () => {
   const {isSubmenuOpen,location,page:{page,links}} = useGlobalContext()
   const container = useRef(null)
+  const [col,setCol]=useState('col-2')
   useEffect(()=>{
+    setCol('col-2')
     const submenu = container.current
     const {center, bottom} =location
     submenu.style.left =`${center}px`
     submenu.style.top =`${bottom}px`
 
-  },[location])
+    if(links.length === 3){
+      setCol('col-3')
+    }
+    if(links.length > 3){
+      setCol('col-4')
+    }
+
+  },[location,links])
   return (
       <aside className={`${isSubmenuOpen? ' submenu show':
         'submenu'}`} ref={container}>
         <h4>{page}</h4>
-        <div className={`submenu-center col-2`}>
+        <div className={`submenu-center ${col}`}>
           {links.map((link,index)=>{
             const {label,icon,url} = link
             return <a key={index} href={url}>
