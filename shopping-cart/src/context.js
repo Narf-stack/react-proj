c4101e6420af7f40ddef4f431054275dc9fbb3fc
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-03-28 20:25:00 +0200

    Remove an Item

--- shopping-cart/src/context.js
+++ shopping-cart/src/context.js
@@ -17,10 +17,13 @@ const AppProvider = ({ children }) => {
     dispatch({type: 'CLEAR_CART'})
   }
 
+  const remove =(id)=>{
+    dispatch({type: 'REMOVE', payload:id})
+  }
   return (
     <AppContext.Provider
       value={{
-        ...state,clearCart
+        ...state,clearCart,remove
       }}
     >
       {children}
