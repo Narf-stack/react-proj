99ec543afca18c006e085fa8dcf8f36871fe4689
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-03-28 20:35:39 +0200

    Increase numb of Item

--- shopping-cart/src/reducer.js
+++ shopping-cart/src/reducer.js
@@ -7,6 +7,15 @@ const reducer = (state,action) =>{
   if(action.type === 'REMOVE'){
     return {...state,cart:state.cart.filter((item)=> item.id !== action.payload)}
   }
+  if(action.type === 'INCREASE'){
+    let tempCart = state.cart.map((item)=>{
+      if( item.id === action.payload){
+        return { ...item,amount:item.amount +1}
+      }
+      return item
+    })
+    return {...state,cart:tempCart}
+  }
   return state
 }
 
