b8c9ba3d3ea513e2706822ee1a8de0dba061228a
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-03-28 20:19:48 +0200

    Clear the cart

--- shopping-cart/src/reducer.js
+++ shopping-cart/src/reducer.js
@@ -1,6 +1,9 @@
 // state: current state before the update
 // action: what we are trying to do
 const reducer = (state,action) =>{
+  if(action.type === 'CLEAR_CART'){
+    return {...state,cart:[]}
+  }
   return state
 }
 
