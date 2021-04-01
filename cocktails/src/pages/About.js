9c967926c1f984df6f01d0f0e3d6ad7046b231cc
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-04-01 15:09:17 +0200

    Seting up About page

--- cocktails/src/pages/About.js
+++ cocktails/src/pages/About.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        For the better part of two centuries, sugar has been
+        the primary conduit of sweetness in cocktails, often
+        in the form of the aptly named simple syrup or dissolved
+        in a cocktail shaker.
+        Modern bartenders have shaken up the mix in recent
+        years with alternatives like maple or agave syrup,
+        jams and complex custom concoctions. But there is one
+        oft overlooked supporting player that’s been in the
+        mixologist’s arsenal for generations, and it sits in
+        most home pantries: honey.
+      </p>
+    </section>
   )
 }
 
