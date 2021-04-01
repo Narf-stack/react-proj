8aeb6c6a87c070945858493f026b92f37f49055d
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-04-01 18:57:57 +0200

    Seting up Searchform

--- cocktails/src/components/SearchForm.js
+++ cocktails/src/components/SearchForm.js
@@ -3,10 +3,31 @@ import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
   const {setSearchTerm} = useGlobalContext()
+  const searchValue = React.useRef('')
+
+  const searchCocktail=()=>{
+    setSearchTerm(searchValue.current.value)
+  }
+
+  React.useEffect(()=>{
+    searchValue.current.focus()
+  },[])
+
+  // Ensure that the app wont render when user hits enter
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+  }
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className='section search'>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='name'> search yr cocktail</label>
+          <input type="text" id='name' ref={searchValue}
+            onChange={searchCocktail}
+           />
+        </div>
+      </form>
+    </section>
   )
 }
 
